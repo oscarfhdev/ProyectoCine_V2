e36b7433d2547990f7ab8aefc0d782be1713d73e
oscarfhdev <oscarfh.dev@gmail.com>
2026-03-19 21:21:55 +0100

    prueba commit

--- src/main/java/_DAM/Cine_V2/security/CineUserDetailsService.java
+++ src/main/java/_DAM/Cine_V2/security/CineUserDetailsService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CineUserDetailsService implements UserDetailsService {
-
+    //prueba
     private final UsuarioRepository repo;
 
     @Override
